6d16d4b64b478e35388c1508bdef76af72818062
Chetan Bargal <100152637+CHAITUBARGAL@users.noreply.github.com>
2025-02-14 17:31:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FileUpload from './components/FileUpload';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Secret Santa Assignment</h1>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
